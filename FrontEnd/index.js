f531881034da35297e3c33acf74577f366b0c594
Nolan Tranchant <Nolan.tranchant@hotmail.fr>
2026-01-29 19:40:34 +0100

    Ajout Modale + Ajout photo

---
+++ FrontEnd/index.js
@@ -0,0 +1,217 @@
+const portfolioFiltres = document.getElementById("portfolio-filtres");
+const galerie = document.querySelector(".gallery");
+
+// Récupère les datas de l'API //
+async function dataTri() {
+  const reponse = await fetch("http://localhost:5678/api/works");
+  const data = await reponse.json();
+  return data;
+}
+
+// Utilise les datas de l'API pour afficher les projets //
+async function afficherTravaux() {
+  const data = await dataTri();
+  afficherGallerie(data);
+}
+
+// Utilise les datas de l'API pour afficher les filtres //
+async function selectionDataFiltres() {
+  //récupère les datas de la function dataTri()//
+  const data = await dataTri();
+  let idFiltres = [];
+  idFiltres.push({ name: "Tous", id: 0 });
+
+  // Pour chaque éléments du tableau data, compare moi avec le tableau idFiltres l'ID//
+  data.forEach((item) => {
+    const existe = idFiltres.some(
+      (idFiltres) => idFiltres.id === item.category.id,
+    );
+
+    // Si les id sont différents, tu me le push dans le tableau//
+    if (!existe) {
+      idFiltres.push({
+        name: item.category.name,
+        id: item.category.id,
+      });
+    }
+  });
+
+  // Affichage des lignes avec l'ID et le nom de chaque catégorie//
+  portfolioFiltres.innerHTML = idFiltres
+    .map(
+      (filtre) =>
+        `<li class="portfolio-filtres_all" data-category =${filtre.id}>${filtre.name}</li>`,
+    )
+    .join("");
+}
+
+function afficherGallerie(tableau) {
+  // vide ma gallerie avant chaque actualisation//
+  galerie.innerHTML = ``;
+
+  // Pour chaque tableau transmis en paramètres, affiche moi ceci//
+  tableau.forEach((item) => {
+    galerie.innerHTML += `<div>
+            <img src=${item.imageUrl} alt=${item.title}></img>
+            <figcaption>${item.title}</figcaption>
+          </div>`;
+  });
+}
+//Mis en place du tri Fonctionnelle //
+
+async function triFonctionnelle() {
+  const data = await dataTri();
+
+  // Filtre et récupère dans un tableau les éléments appartennant au même id //
+  const triObjet = data.filter((objet) => objet.category.id === 1);
+  const triAppartements = data.filter((objet) => objet.category.id === 2);
+  const triHotel = data.filter((objet) => objet.category.id === 3);
+
+  portfolioFiltres.addEventListener("click", (e) => {
+    // récupère le data-category de mes <li> //
+    const cible = e.target.dataset.category;
+    console.log(cible);
+    // Distingue les 4 catégories différentes pour l'affichage//
+    switch (cible) {
+      case "0":
+        afficherGallerie(data);
+        break;
+      case "1":
+        afficherGallerie(triObjet);
+        break;
+      case "2":
+        afficherGallerie(triAppartements);
+        break;
+      case "3":
+        afficherGallerie(triHotel);
+        break;
+    }
+  });
+}
+
+function loginActif() {
+  const header = document.querySelector("header");
+  const login = document.querySelector(".nav-login");
+  const titreProjets = document.querySelector(".mes-projets");
+  const btnProjets = document.createElement("button");
+
+  //Création d'éléments du DOM //
+  const divHead = document.createElement("div");
+  const icone = document.createElement("i");
+  const iconeProjets = document.createElement("i");
+
+  const spanBtn = document.createElement("span");
+
+  const spanHead = document.createElement("span");
+  spanHead.textContent = "Mode édition";
+
+  //Ajout de class //
+  spanHead.classList.add("header-admin_span");
+  icone.classList.add("fa-solid", "fa-pen-to-square");
+  iconeProjets.classList.add("fa-solid", "fa-pen-to-square");
+  divHead.classList.add("header-admin");
+  spanBtn.textContent = "modifier";
+
+  // Rattacher les enfants aux parents//
+  divHead.appendChild(icone);
+  divHead.appendChild(spanHead);
+  btnProjets.appendChild(iconeProjets);
+  btnProjets.appendChild(spanBtn);
+
+  if (localStorage.getItem("token")) {
+    header.appendChild(divHead);
+    login.textContent = "logout";
+    portfolioFiltres.classList.add("hide");
+    titreProjets.appendChild(btnProjets);
+
+    login.addEventListener("click", (e) => {
+      e.preventDefault();
+      localStorage.removeItem("token");
+      login.textContent = "login";
+      divHead.remove();
+      iconeProjets.remove();
+      btnProjets.remove();
+      portfolioFiltres.classList.remove("hide");
+      window.location.href = "index.html";
+    });
+  }
+}
+
+async function modale() {
+  const data = await dataTri();
+  const divGallerie = document.createElement("div")
+  const btnProjets = document.querySelector("button");
+  const mesProjets = document.querySelector(".mes-projets");
+
+  // Ecoute le bouton "modifier" et injecte du HTML au click pour former la modale //
+  btnProjets.addEventListener("click", (e) => {
+    mesProjets.appendChild(divGallerie)
+    divGallerie.innerHTML = `<div class='modale'>
+<i class="fa-solid fa-xmark"></i>
+<h2>Galerie photo</h2>
+<ul class="modale-photos">
+          ${affichageGallerieModale(data)}
+        </ul>
+<button class="modale-button">Ajouter une photo</button>
+
+</div>`;
+
+//Ecoute du bouton qui ferme la modale //
+  const btnFermer = document.querySelector(".fa-xmark");
+  btnFermer.addEventListener("click", (e) => {
+      if (e.target.classList.contains("fa-xmark")) {
+        divGallerie.innerHTML="";
+      }
+    });
+
+
+    const modaleBouton = document.querySelector(".modale-button")
+    modaleBouton.addEventListener("click", (e) => {
+      divGallerie.innerHTML =
+      `<div class='modale'>
+          <i class="fa-solid fa-arrow-left"></i>
+          <i class="fa-solid fa-xmark"></i>
+          <h2>Ajout photo</h2>
+          <form>
+            <div class='form-ajout'>
+              <i class="fa-regular fa-image"></i>
+              <button>+ Ajouter photo</button>
+              <p>jpg.png: 4mo max</p>
+            </div>
+            <div class='form-formulaire'>
+              <label for='titre'>Titre</label>
+              <input type='text' name='titre' class='form-input'></input>
+              <label for='categorie'>Catégorie</label>
+              <select name='categorie' class='form-input'>
+                <option></option>
+                <option>Objets</option>
+                <option>Appartements</option>
+                <option>Hôtel</option>
+              </select>
+            </div>
+              <button class='modale-form_button'>Valider</button>
+          </form>
+      </div>`;
+    })
+  });
+}
+
+function affichageGallerieModale(data) {
+  return data
+    .map(
+      (item) => `
+        <li class="modale-photos_all">
+          <i class="fa-solid fa-trash-can"></i>
+          <img src="${item.imageUrl}" alt="${item.title}">
+        </li>
+      `,
+    )
+    .join("");
+}
+
+afficherTravaux();
+selectionDataFiltres();
+triFonctionnelle();
+loginActif();
+
+modale();
---
+++ FrontEnd/login.js
@@ -0,0 +1,38 @@
+function login() {
+  const formulaire = document.getElementById("login");
+  const title = document.querySelector(".form-title");
+
+  formulaire.addEventListener("submit", (e) => {
+    // Bloque le rechargement automatique de la page//
+    e.preventDefault();
+
+    //Construction de la charge Utile pour l'envoie des données//
+    const infoFormulaire = {
+      email: e.target.querySelector("[name=mail]").value,
+      password: e.target.querySelector("[name=mdp]").value,
+    };
+
+    const chargeUtile = JSON.stringify(infoFormulaire);
+    console.log(chargeUtile);
+    fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: chargeUtile,
+    })
+      //Récupération de la réponse et convertis en format JSON//
+      .then((reponse) => {
+        return reponse.json();
+      })
+      //récupération du Token de confirmation//
+      .then((data) => {
+        if (data.token) {
+          localStorage.setItem("token", data.token);
+          window.location.href = "index.html";
+        } else {
+          title.innerHTML = `<h2>Log In</h2><p>Votre mail ou mot de passe est incorrect</p>`;
+        }
+      });
+    //   .catch((erreur) => console.log(erreur));
+  });
+}
+login();
